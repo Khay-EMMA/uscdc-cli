de40f97aed656d855443159d0092f982c6f23fcc
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-08-19 04:10:42 +0100

    Update:🔥💵Implement transfer function💵

--- scripts/mintUsdc.js
+++ scripts/mintUsdc.js
@@ -8,10 +8,19 @@ const main = async () => {
   const usdc = new ethers.Contract(address, abi, account);
 
   const tx = await usdc.gimmeSome({ gasPrice: 20e9 });
-
-  console.log(`Transaction Hash: ${tx.hash}`);
-  const receipt = tx.wait();
-  console.log(`Transaction confirmed in block number: ${receipt.blockNumber}`);
-  console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+  try {
+    console.log(`Transaction Hash: ${tx.hash}`);
+  } catch (e) {
+    console.log(e);
+  }
+  const receipt = await tx.wait();
+  try {
+    console.log(
+      `Transaction confirmed in block number: ${receipt.blockNumber}`
+    );
+    console.log(`Gas used: ${receipt.gasUsed.toString()}`);
+  } catch (e) {
+    console.log(e);
+  }
 };
 main();
